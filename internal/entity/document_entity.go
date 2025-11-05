30bfad5f70687aeff1d5e648f9bb8ba614c59a95
Azka rizqullah <azkarizqullah99@gmail.com>
2025-11-05 11:58:22 +0700

    fix: model document

--- internal/entity/document_entity.go
+++ internal/entity/document_entity.go
@@ -9,20 +9,18 @@ import (
 type Document struct {
 	ID                       uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
 	DocumentUrl              *string   `json:"document_url" gorm:""`
-	WBS                      string    `json:"wp" gorm:"not null"`
 	DocumentSerialNumber     string    `json:"document_serial_number" gorm:"not null"`
-	CompanyDocumentNumber    string    `json:""`
-	ContractorDocumentNumber string    `json:"document_number" gorm:"not null"`
+	CTRNumber                string    `json:"ctr_number" gorm:"not null"`
+	WBS                      string    `json:"wbs" gorm:"not null"`
+	CompanyDocumentNumber    string    `json:"company_document_number"`
+	ContractorDocumentNumber string    `json:"contractor_document_number" gorm:"not null"`
 	DocumentTitle            string    `json:"document_title" gorm:"not null"`
 	Discipline               string    `json:"discipline" gorm:"not null"`
 	SubDiscipline            *string   `json:"sub_discipline" gorm:""`
 	DocumentType             string    `json:"document_type" gorm:"not null"`
 	DocumentCategory         string    `json:"document_category" gorm:"not null"`
-	DocumentDate             string    `json:"document_date" gorm:"not null"`
 
-	Project string `json:"project" gorm:"not null"`
-	Lookup  string `json:"lookup" gorm:"not null"`
-	Status  string `json:"status" gorm:"not null"`
+	Status string `json:"status" gorm:"not null"`
 
 	Deadline time.Time `json:"deadline" gorm:"not null"`
 
