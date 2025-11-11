d1d05b8f202618feda9c3db5ec53421dafd68b31
Azka rizqullah <azkarizqullah99@gmail.com>
2025-11-11 09:14:30 +0700

    fix:

--- internal/api/routes/comment_route.go
+++ internal/api/routes/comment_route.go
@@ -10,7 +10,7 @@ import (
 func Comment(app *gin.Engine, commentcontroller controller.CommentController, middleware middleware.Middleware) {
 	routes := app.Group("/api/v1/area-of-concern-group/:area_of_concern_group_id/area-of-concern/:area_of_concern_id/comment")
 	{
-		routes.POST("", middleware.Authenticate(), middleware.OnlyAllow(string(entity.RoleSuperAdmin), string(entity.RoleContractor)), commentcontroller.Create)
+		routes.POST("", middleware.Authenticate(), middleware.OnlyAllow(string(entity.RoleSuperAdmin), string(entity.RoleReviewer)), commentcontroller.Create)
 		routes.POST("/:comment_id/reply", middleware.Authenticate(), commentcontroller.ReplyId)
 
 		routes.GET("", middleware.Authenticate(), commentcontroller.GetAllByAreaOfConcernId)
